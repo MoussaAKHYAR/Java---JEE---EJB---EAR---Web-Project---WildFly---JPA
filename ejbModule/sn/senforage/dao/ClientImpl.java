36202b14c26769ed3ae3626c282163e5a324bf95
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-10-21 14:17:53 +0000

    creation des session Bean

--- ejbModule/sn/senforage/dao/ClientImpl.java
+++ ejbModule/sn/senforage/dao/ClientImpl.java
@@ -3,28 +3,51 @@ package sn.senforage.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import sn.senforage.entities.Client;
 
 @Stateless
 public class ClientImpl implements IClient{
 
-	@Override
-	public int add(Client client) {
-		// TODO Auto-generated method stub
-		return 0;
+private EntityManager em;
+	
+    public ClientImpl() {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("senforagePU");
+		em = emf.createEntityManager();
 	}
 
 	@Override
-	public List<Client> clients() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public int add(Client client) {
+		try {
+			em.getTransaction().begin();
+			em.persist(client);
+			em.getTransaction().commit();
+			return 1;
+		}catch (Exception ex){
+			ex.printStackTrace();
+			return  0;
+		}
+    }
+
+    @Override
+    public List<Client> clients() {
+    	return em.createQuery("SELECT c FROM Client c").getResultList();    }
 
 	@Override
 	public int delete(int id, Client client) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		try {
+			em.getTransaction().begin();
+			client = em.find(Client.class,id);
+			em.remove(client);
+			em.getTransaction().commit();
+			return 1;
+		}catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return 0;
+		}	}
 
 }
--- ejbModule/sn/senforage/dao/VillageImpl.java
+++ ejbModule/sn/senforage/dao/VillageImpl.java
@@ -3,40 +3,73 @@ package sn.senforage.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import sn.senforage.entities.Village;
 
 @Stateless
 public class VillageImpl implements IVillage {
 
+	private EntityManager em;
+
+	public VillageImpl() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("SenforageEJB");
+		em = emf.createEntityManager();
+	}
 	@Override
 	public int add(Village village) {
-		// TODO Auto-generated method stub
-		return 0;
+		try {
+			em.getTransaction().begin();
+			em.persist(village);
+			em.getTransaction().commit();
+			return 1;
+		}catch (Exception ex){
+			ex.printStackTrace();
+			return  0;
+		}
 	}
 
 	@Override
 	public List<Village> villages() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int delete(int id, Village village) {
-		// TODO Auto-generated method stub
-		return 0;
+		return em.createQuery("SELECT v FROM Village v").getResultList();
 	}
 
 	@Override
 	public Village getVillageByName(String nomVillage) {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		try {
 
+			Village village = (Village) em
+					.createQuery("select v from Village v where v.nomVillage=:n")
+					.setParameter("n",nomVillage)
+					.getSingleResult();
+			return village;
+
+		}catch (Exception e)
+		{
+			return null;
+		}
+	}
+	@Override
+	public int delete(int id, Village village) {
+		try {
+			em.getTransaction().begin();
+			village = em.find(Village.class,id);
+			em.remove(village);
+			em.getTransaction().commit();
+			return 1;
+		}catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return 0;
+		}
+	}
 	@Override
 	public Village get(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		return em.find(Village.class,id);
 	}
 
 }
